cb0ac91fccdddc87c9cb50dee284ec2d829090d2
Jose Pablo <jose.gil@utec.edu.pe>
2024-11-23 07:29:14 +0000

    dh corregido. control cinematico de posicion completo. control cinematico de orientacion incompleto (no es necesario). instalacion de rbdl e implementacion de codigo de control dinamico (falta corregir).

--- proyecto/src/prfuncs.py
+++ proyecto/src/prfuncs.py
@@ -1,8 +1,31 @@
 import numpy as np
 from copy import copy
 from pyquaternion import Quaternion
+import rbdl
 
 pi = np.pi
+
+class Robot(object):
+    def __init__(self, q0, dq0, ndof, dt):
+        self.q = q0    # numpy array (ndof x 1)
+        self.dq = dq0  # numpy array (ndof x 1)
+        self.M = np.zeros([ndof, ndof])
+        self.b = np.zeros(ndof)
+        self.dt = dt
+        self.robot = rbdl.loadModel('/home/user/proy_ws/src/robot_description/urdf/robot_description.urdf')
+
+    def send_command(self, tau):
+        rbdl.CompositeRigidBodyAlgorithm(self.robot, self.q, self.M)
+        rbdl.NonlinearEffects(self.robot, self.q, self.dq, self.b)
+        ddq = np.linalg.inv(self.M).dot(tau-self.b)
+        self.q = self.q + self.dt*self.dq
+        self.dq = self.dq + self.dt*ddq
+
+    def read_joint_positions(self):
+        return self.q
+
+    def read_joint_velocities(self):
+        return self.dq
 
 # Matriz de transformación homogénea a partir de parámetros de Denavit-Hartenberg
 def dh(d, theta, a, alpha):
@@ -22,10 +45,12 @@ def fkine(q):
  T1 = dh(0.3231,   pi/2+q[0],  0.0,    pi/2)
  T2 = dh(0.0,      pi/2+q[1],  0.2105, 0.0)
  T3 = dh(0.0,      pi/2+q[2],  0.0,    pi/2)
- T4 = dh(0.2641,   pi+q[3],    0.0,    pi/2)
- T5 = dh(0.0,      pi+q[4],    0.0,    pi/2)
- T6 = dh(0.1636,   q[5],       0.0,    0.0)
- T = T1 @ T2 @ T3 @ T4 @ T5 @ T6
+ T4 = dh(q[3],     0.0,        0.0,    0.0)
+ T5 = dh(0.4263,   pi+q[4],    0.0,    pi/2)
+
+ T6 = dh(0.0,      pi+q[5],    0.0,    pi/2)
+ T7 = dh(0.1636,   q[6],       0.0,    0.0)
+ T = T1 @ T2 @ T3 @ T4 @ T5 @ T6 @ T7
  return T
 
 def TF2xyzquat(T):
